ae6d78edca1ca8422ea8b39d2a94638f5c1596e5
Richard Tape <richard.tape@ubc.ca>
2023-08-23 16:21:17 -0700

    Updated for PHP 8.2

--- wave-resize-image.php
+++ wave-resize-image.php
@@ -1,17 +1,17 @@
 <?php
 /*
-Plugin Name: Wave Resize Image 
+Plugin Name: Wave Resize Image
 Plugin URI: github.com/ubc/
-Description: Enables us to resize the images with a photon like api 
+Description: Enables us to resize the images with a photon like api
 Version: 0.2
 Author: Enej
 Author URI: http://ctlt.ubc.ca
 */
 
-/* Implementation 
- 1. add the following to the htaccess file 
- 
-#Send images to timthumb to be resized 
+/* Implementation
+ 1. add the following to the htaccess file
+
+#Send images to timthumb to be resized
 RewriteCond %{REQUEST_FILENAME} -f
 RewriteCond %{REQUEST_URI} \.(gif|png|jpg|jpeg)
 RewriteCond %{QUERY_STRING} (b|w|h|zc|q|a)=(.*)$
@@ -28,34 +28,34 @@ and the folder wave-resize-image as well into the mu-plugins directory
 
 /**
  * wave_resize_image_url function.
- * 
+ *
  * @access public
  * @param mixed $url
  * @param mixed $options
- * @return void
+ * @return string
  */
 function wave_resize_image_url( $url, $args ) {
 	$blog_id = get_current_blog_id();
-	
+
 	extract( shortcode_atts( array(
 		'width' => 150,
 		'height' => null,
 		'zc' => 1,
 		'blog_id'=> $blog_id
 	), $args ) );
-	
+
 	$exploded = explode( 'files', $url);
 
 	$height = 	filter_var( $height, FILTER_SANITIZE_NUMBER_INT );
 	$width 	= 	filter_var( $width, FILTER_SANITIZE_NUMBER_INT );
 	$h = ( isset( $height ) ? '&h='.$height : '' );
-	
-	// we don't find 
+
+	// we don't find
 	if( $url == $exploded[0] ) {
 		return esc_url( $url."?w=".$width.$h.'&zc='.$zc );
-		
+
 	}
-	
+
 
 	if( defined( 'SUBDOMAIN_INSTALL') &&  SUBDOMAIN_INSTALL )
 		return get_site_url().esc_url( "/wp-content/blogs.dir/".$blog_id."/files".$exploded[1]."?b=".$blog_id."&w=".$width.$h.'&zc='.$zc );
@@ -65,19 +65,19 @@ function wave_resize_image_url( $url, $args ) {
 
 /**
  * wave_resize_featured_image function.
- * 
+ *
  * @access public
  * @param mixed $post_id
  * @param mixed $width
  * @param mixed $height (default: null)
  * @param int $zc (default: 1)
- * @return void
+ * @return mixed
  */
 function wave_resize_featured_image_url( $post_id, $width, $height=null, $zc=1 ) {
-	$attachment_id = get_post_thumbnail_id( $post_id );	
+	$attachment_id = get_post_thumbnail_id( $post_id );
 	$url = wp_get_attachment_image_src( $attachment_id,'full', false );
-	$args = array( 
-		'width' => $width, 
+	$args = array(
+		'width' => $width,
 		'height' => $height,
 		'zc' => $zc
 	);
@@ -86,13 +86,13 @@ function wave_resize_featured_image_url( $post_id, $width, $height=null, $zc=1 )
 
 /**
  * wave_resize_featured_image function.
- * 
+ *
  * @access public
  * @param mixed $post_id
  * @param mixed $width
  * @param mixed $height (default: null)
  * @param int $zc (default: 1)
- * @return void
+ * @return mixed
  */
 function wave_resize_featured_image($post_id, $width, $height=null, $zc=1 ) {
 
